8302d78cd0201f8333c960a6a12b76929e435dc9
ariskafebrilianti <bncliany@gmail.com>
2023-03-08 21:19:49 +0700

    update

---
+++ fizzbuzz!.js
@@ -0,0 +1,17 @@
+function result() {
+    let inputMultiple = document.getElementById('multiple').value;
+
+    let finalRes = ""
+    if(inputMultiple % 15 === 0) {
+        finalRes = "FizzBuzz!"
+    } else if (inputMultiple % 5 === 0){
+        finalRes = "Buzz!"
+    } else if (inputMultiple % 3 === 0) {
+        finalRes = "Fizz!"
+    } else if (inputMultiple){
+        finalRes = inputMultiple
+    }
+    const value = document.createElement("p")
+    value.innerHTML = finalRes
+    document.getElementById('display').appendChild(value);
+}
