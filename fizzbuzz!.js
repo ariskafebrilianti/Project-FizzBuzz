a41ff265408c65ff016875156baaae8541bb675d
ariskafebrilianti <bncliany@gmail.com>
2023-03-09 21:50:06 +0700

    update2

--- fizzbuzz!.js
+++ fizzbuzz!.js
@@ -2,13 +2,15 @@ function result() {
     let inputMultiple = document.getElementById('multiple').value;
 
     let finalRes = ""
-    if(inputMultiple % 15 === 0) {
+    if (inputMultiple == 0) {
+        finalRes = "Invalid Input"
+    } else if (inputMultiple % 15 === 0) {
         finalRes = "FizzBuzz!" 
     } else if (inputMultiple % 5 === 0){
         finalRes = "Buzz!"
     } else if (inputMultiple % 3 === 0) {
         finalRes = "Fizz!"
-    } else if (inputMultiple){
+    } else if (inputMultiple) { 
         finalRes = inputMultiple
     }
     const value = document.createElement("p")
